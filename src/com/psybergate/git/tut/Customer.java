ca21f1ed5dcecc1c152b8cfc007a7252633908dd
Tshepo Toloane <tshepo@psybergate.co.za>
2019-06-01 14:45:22 +0200

    added files

---
+++ src/com/psybergate/git/tut/Customer.java
@@ -0,0 +1,19 @@
+public class Customer{
+	
+	public String getName(){
+		return "name";
+	}
+	
+	public String getDate(){
+		return "date";
+	}
+	
+	public String getNumber(){
+		return "number";
+	}
+	
+	public String toString(){
+		return "Customer::toString()";
+	}
+	
+}
